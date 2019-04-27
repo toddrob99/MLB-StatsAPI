e1ae878fb2857a1d81ea888222c5ded19b129211
todd <toddrob@gmail.com>
2019-04-27 09:34:44 -0400

    build out roster()

--- statsapi/__init__.py
+++ statsapi/__init__.py
@@ -668,6 +668,40 @@ def roster(teamId,rosterType=None,season=None,date=None):
     Get a list of available rosterTypes by calling the meta endpoint with type=rosterTypes.
     Default rosterType=active
     Format for date = 'MM/DD/YYYY', e.g. '04/24/2019'
+
+    Example use:
+    
+    Print the current Phillies active roster:
+
+    print( statsapi.roster(143) )
+
+    Output:
+
+    #23  CF  Aaron Altherr
+    #27  P   Aaron Nola
+    #46  P   Adam Morgan
+    #15  C   Andrew Knapp
+    #22  RF  Andrew McCutchen
+    #3   RF  Bryce Harper
+    #16  2B  Cesar Hernandez
+    #25  RF  Dylan Cozens
+    #61  P   Edubray Ramos
+    #51  P   Enyel De Los Santos
+    #50  P   Hector Neris
+    #10  C   J.T. Realmuto
+    #49  P   Jake Arrieta
+    #48  P   Jerad Eickhoff
+    #52  P   Jose Alvarez
+    #12  P   Juan Nicasio
+    #7   3B  Maikel Franco
+    #5   RF  Nick Williams
+    #93  P   Pat Neshek
+    #9   2B  Phil Gosselin
+    #17  1B  Rhys Hoskins
+    #13  2B  Sean Rodriguez
+    #58  P   Seranthony Dominguez
+    #21  P   Vince Velasquez
+    #56  P   Zach Eflin
     """
     if not season: season = datetime.now().year
     if not rosterType: rosterType='active'
@@ -676,9 +710,15 @@ def roster(teamId,rosterType=None,season=None,date=None):
 
     r = get('team_roster',params)
 
-    
+    roster = ''
+    players = []
+    for x in r['roster']:
+        players.append([x['jerseyNumber'],x['position']['abbreviation'],x['person']['fullName']])
 
-    return "This function is not yet available."
+    for i in range(0,len(players)):
+        roster += ('#{:<3} {:<3} {}\n').format(*players[i])
+
+    return roster
 
 def meta(type,fields=None):
     """Get available values from StatsAPI for use in other queries,
