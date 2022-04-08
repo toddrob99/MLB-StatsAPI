51e63c0d37ac1a7a34bd93fe8e658e4b223d0a33
todd <toddrob@gmail.com>
2022-04-08 14:58:12 -0400

    chore: automatically build and deploy to pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 import setuptools
-from statsapi import version
+from distutils.util import convert_path
+
+# https://stackoverflow.com/questions/2058802/how-can-i-get-the-version-defined-in-setup-py-setuptools-in-my-package
+main_ns = {}
+ver_path = convert_path("statsapi/version.py")
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="MLB-StatsAPI",
-    version=version.VERSION,
+    version=main_ns["VERSION"],
     author="Todd Roberts",
     author_email="todd@toddrob.com",
     description="MLB Stats API Wrapper for Python",
