3e913d2768c6db733a404a0fc07fe5e63a61fd86
jimcreel <jim.creel@gmail.com>
2023-07-19 00:11:27 -0700

    removed print logging

--- statsapi/__init__.py
+++ statsapi/__init__.py
@@ -1448,9 +1448,7 @@ def standings_data(
     standingsTypes=None,
     date=None,
 ):
-    print("getting standings data")
-    print("division")
-    print(division)
+    
     """Returns a dict of standings data for a given league/division and season."""
     params = {"leagueId": leagueId}
     if date:
@@ -1473,13 +1471,12 @@ def standings_data(
         }
     )
 
-    print(params)
     r = get("standings", params)
 
     divisions = {}
 
     for y in r["records"]:
-        print(y["division"]["id"])
+
         for x in (
             x
             for x in y["teamRecords"]
@@ -1488,7 +1485,6 @@ def standings_data(
             or str(division) == str(x["team"]["division"]["id"])
             
         ):
-            print(x)
             if x["team"]["division"]["id"] not in divisions.keys():
                 divisions.update(
                     {
