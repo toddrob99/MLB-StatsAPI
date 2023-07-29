e15033097cd6917c459aafe89c22634c8ac2a34b
todd <toddrob@gmail.com>
2023-07-29 14:35:07 -0400

    enh: support division id in standings() and standings_data() #115 ver: 1.6.2

--- statsapi/__init__.py
+++ statsapi/__init__.py
@@ -1440,7 +1440,6 @@ def standings(
 
 
 def standings_data(
-    
     leagueId="103,104",
     division="all",
     include_wildcard=True,
@@ -1448,7 +1447,6 @@ def standings_data(
     standingsTypes=None,
     date=None,
 ):
-    
     """Returns a dict of standings data for a given league/division and season."""
     params = {"leagueId": leagueId}
     if date:
@@ -1480,10 +1478,9 @@ def standings_data(
         for x in (
             x
             for x in y["teamRecords"]
-            if division.lower() == "all"
-            or division.lower() == x["team"]["division"]["abbreviation"].lower()
+            if str(division).lower() == "all"
+            or str(division).lower() == x["team"]["division"]["abbreviation"].lower()
             or str(division) == str(x["team"]["division"]["id"])
-            
         ):
             if x["team"]["division"]["id"] not in divisions.keys():
                 divisions.update(
